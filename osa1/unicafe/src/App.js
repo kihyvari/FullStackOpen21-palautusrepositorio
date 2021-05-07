44b6a658f00c17cb5653ef1bb0ad01e4cd5255c8
Kammo Haverinen <kihyvari@gmail.com>
2021-05-07 09:00:12 +0300

    Lisää tehtävät 1.7-1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -5,12 +5,18 @@ const Button = ({ handleClick, text }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+	const all = good + neutral + bad
+	const average = (good - bad)/all
+	const positive = good/all*100
 	return (
 		<div>
 			<h1>statistics</h1>
 			<p>good {good}</p>
 			<p>neutral {neutral}</p>
 			<p>bad {bad}</p>
+			<p>all {all}</p>
+			<p>average {average}</p>
+			<p>positive {positive} %</p>
 		</div>
 	);
 };
